0e05365398dd416648498bee97fdac6c8122f7f8
horapcode <hohuuhuy208@gmail.com>
2026-07-17 10:49:56 +0700

    feat

--- src/Components/Case Study/CaseStudy.jsx
+++ src/Components/Case Study/CaseStudy.jsx
@@ -6,7 +6,7 @@ const CaseStudy = () => {
   const caseStudyData = [
     "For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.",
     "For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.",
-    "For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.",
+    "For a  national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.",
   ];
   return (
     <section
